8f195faa57fb19796cda80f0c0f4567be8926711
Felipe Jose <felipe.jsilva290196@gmail.com>
2021-11-05 14:43:40 -0300

    Group by search

--- backend/src/main/java/com/felipe/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/felipe/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.felipe.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.dsvendas.dto.SaleDTO;
+import com.felipe.dsvendas.dto.SaleSuccessDTO;
+import com.felipe.dsvendas.dto.SaleSumDTO;
 import com.felipe.dsvendas.service.SaleService;
 
 @RestController
@@ -21,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGoupedBySeller(){
+		List<SaleSumDTO> list = service.amountGoupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGoupedBySeller(){
+		List<SaleSuccessDTO> list = service.successGoupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 
--- backend/src/main/java/com/felipe/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/felipe/dsvendas/service/SaleService.java
@@ -1,6 +1,8 @@
 package com.felipe.dsvendas.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.felipe.dsvendas.dto.SaleDTO;
+import com.felipe.dsvendas.dto.SaleSuccessDTO;
+import com.felipe.dsvendas.dto.SaleSumDTO;
 import com.felipe.dsvendas.entities.Sale;
 import com.felipe.dsvendas.repositories.SaleRepository;
 import com.felipe.dsvendas.repositories.SellerRepository;
@@ -27,4 +31,14 @@ public class SaleService {
 		Page<Sale> result =  repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public 	List<SaleSumDTO> amountGoupedBySeller(){
+		return repository.amountGoupedBySeller();
+	}
+	@Transactional(readOnly = true)
+	public 	List<SaleSuccessDTO> successGoupedBySeller(){
+		return repository.successGoupedBySeller();
+	}
+
 }
